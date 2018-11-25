91730b4e1a00afe220be867cd4b2aa65e7e103f4
ChristinaLavingia <christinalavingia@gmail.com>
2018-11-25 15:05:53 -0800

    Final code with working reset function and scores.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,50 +20,92 @@
     var yellowGem = Math.floor(Math.random() * 11 + 1);
     var greenGem = Math.floor(Math.random() * 11 + 1);
 
-    console.log(pinkGem);
+//Resets the game after a win or loss
 
-//Adds the value of each crystal clicked and prints to the DOM
+    function reset() {
+        randNum = Math.floor(Math.random()* 101 + 19);
+        $("#randNumber").text(randNum);
+        pinkGem = Math.floor(Math.random() * 11 + 1);
+        blueGem = Math.floor(Math.random() * 11 + 1);
+        yellowGem = Math.floor(Math.random() * 11 + 1);
+        greenGem = Math.floor(Math.random() * 11 + 1);
+        totalPoints = 0;
+        $("#userScore").text("");
+    } 
+
+//Adds the value of the pink crystal and prints to the DOM
 
     $("#pink").on("click", function() {
         totalPoints = totalPoints + pinkGem;
         $("#userScore").text(totalPoints);
-    });
-
-    $("#blue").on("click", function() {
-        totalPoints = totalPoints + blueGem;
-        $("#userScore").text(totalPoints);
-    });
-
-    $("#yellow").on("click", function() {
-        totalPoints = totalPoints + yellowGem;
-        $("#userScore").text(totalPoints);
-    });
-
-    $("#green").on("click", function() {
-        totalPoints = totalPoints + greenGem;
-        $("#userScore").text(totalPoints);
-    });
-
-//Sets wins if the random number equals the total value of the user clicks
 
     if (totalPoints === randNum) {
         win++;
         $("#winCount").text(win);
-        randNum = Math.floor(Math.random()* 101 + 19);
-        var pinkGem = Math.floor(Math.random() * 11 + 1);
-        var blueGem = Math.floor(Math.random() * 11 + 1);
-        var yellowGem = Math.floor(Math.random() * 11 + 1);
-        var greenGem = Math.floor(Math.random() * 11 + 1);
+        reset();
+
     } else if (totalPoints > randNum) {
         loss++;
         $("#lossCount").text(loss);
-        randNum = Math.floor(Math.random()* 101 + 19);
-        var pinkGem = Math.floor(Math.random() * 11 + 1);
-        var blueGem = Math.floor(Math.random() * 11 + 1);
-        var yellowGem = Math.floor(Math.random() * 11 + 1);
-        var greenGem = Math.floor(Math.random() * 11 + 1);
+        reset();
         }
     });
+
+//Adds the value of the blue crystal and prints to the DOM
+
+    $("#blue").on("click", function() {
+        totalPoints = totalPoints + blueGem;
+        $("#userScore").text(totalPoints);
+
+    if (totalPoints === randNum) {
+        win++;
+        $("#winCount").text(win);
+        reset();
+
+    } else if (totalPoints > randNum) {
+        loss++;
+        $("#lossCount").text(loss);
+        reset();
+        }
+    });
+
+//Adds the value of the yellow crystal and prints to the DOM
+
+    $("#yellow").on("click", function() {
+        totalPoints = totalPoints + yellowGem;
+        $("#userScore").text(totalPoints);
+
+    if (totalPoints === randNum) {
+        win++;
+        $("#winCount").text(win);
+        reset();
+
+    } else if (totalPoints > randNum) {
+        loss++;
+        $("#lossCount").text(loss);
+        reset();
+        }
+    });
+
+//Adds the value of the green crystal and prints to the DOM
+
+    $("#green").on("click", function() {
+        totalPoints = totalPoints + greenGem;
+        $("#userScore").text(totalPoints);
+
+    if (totalPoints === randNum) {
+        win++;
+        $("#winCount").text(win);
+        reset();
+
+    } else if (totalPoints > randNum) {
+        loss++;
+        $("#lossCount").text(loss);
+        reset();
+        }
+    });
+
+});
 
 
 
